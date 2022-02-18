07cb0654dcabeb2b5da0e3ba546abef538538764
Pranay-Chopra <pranaychopra2007@gmail.co>
2022-02-18 22:18:20 +0530

    add files

---
+++ commands/help.js
@@ -0,0 +1,16 @@
+exports.run = async (client, message) => {
+
+    message.channel.send({
+        embed: {
+            color: '#E70000',
+            author: { name: 'BOOMBOX Help' },
+            footer:{ text: 'Created by Codemasters' },
+            fields: [
+                { name: 'Bot Commands', value: '`play`, `pause`, `resume`, `queue`, `clear-queue`, `shuffle`, `pb`, `loop`, `volume`, `skip`, `stop`, `ping`' },
+            ],
+            timestamp: new Date(),
+            description: `__Help Menu__`,
+        },
+    });
+
+};
