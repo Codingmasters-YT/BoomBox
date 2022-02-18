d454083fe01d5363c33aa606b9b7431b0c6bc84c
Coding Masters-YT <codingsforlife@gmail.com>
2022-02-18 23:02:47 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -6,7 +6,7 @@ exports.run = async (client, message) => {
             author: { name: 'BOOMBOX Help' },
             footer:{ text: 'Created by Codemasters' },
             fields: [
-                { name: 'Bot Commands', value: '`play`, `pause`, `resume`, `queue`, `clear-queue`, `shuffle`, `pb`, `loop`, `volume`, `skip`, `stop`, `ping`' },
+                { name: 'Bot Commands', value: '`play`, `pause`, `resume`, `queue`, `clear-queue`, `shuffle`, `pb`, `loop`, `volume`, `skip`, `stop`, `ping`, `invite`' },
             ],
             timestamp: new Date(),
             description: `__Help Menu__`,
